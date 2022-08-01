5df7d947e36a42df63ebb62b01e938bc4852405e
BYCengel <burakyavuzcengel@gmail.com>
2022-08-01 16:12:10 +0300

    Initial commit

---
+++ LMS_funct.m
@@ -0,0 +1,87 @@
+clc;
+clear;
+close all;
+
+% Getting inputs
+answer = inputdlg({'Frequency 1','Frequency 2','Frequency 3','Frequency 4', 'Max Gain', 'Psat'},'Please input frequency spacings and max gain');
+frequencys = [str2double(answer{1}),str2double(answer{2}),str2double(answer{3}),str2double(answer{4})];
+g = str2double(answer{5});
+Psat = str2double(answer{6});
+
+% Program code of recording audio
+z=audiorecorder(20000,16,1);
+h=msgbox('Speak Up, I am Recording...','Recording');
+recordblocking(z,5); %Records a 5 sec audio
+delete(h);
+x = getaudiodata(z);
+
+% Block to play audio and corresponding graph
+promptMessage=sprintf('Do want to play your recorded file?');
+titleBarCaption='Play';
+button = questdlg(promptMessage,titleBarCaption,'Yes', 'No','Yes');
+if strcmpi(button,'Yes')
+play(z);
+else
+end
+figure;
+    plot(x);
+    title('Recorded Sound');
+
+% Adding random noise
+Fs = 20000;
+Ts = 1/Fs;
+order = 12;
+t = 0:Ts:1-Ts;
+noise = 0.015*randn(size(x));
+x = x+noise;
+x = x/max(x);
+
+% Block to play audio and corresponding graph
+promptMessage=sprintf('Do want to play your noisy file?');
+titleBarCaption='Play';
+button = questdlg(promptMessage,titleBarCaption,'Yes', 'No','Yes');
+if strcmpi(button,'Yes')
+soundsc(x,Fs);
+else
+end
+figure;
+    plot(x);
+    title('Noise Added Sound');
+% Low pass filter and noise reduction
+b = fir1(order,0.2,'low');
+d = filter(b,1,x);
+mu = 0.03;
+lms = dsp.LMSFilter(order+1, 'StepSize', mu, 'WeightsOutputPort', true);
+[y,e,w] = step(lms, x,d);
+stem([b.' w]); title('System View of LMS Filter');
+hold;
+
+
+figure;
+    plot(y);
+    title('No Gain Added Signal');
+
+% Block to play audio for testing how noise reduction filter works
+promptMessage=sprintf('Play the filtered signal without gain implemented?');
+titleBarCaption='Play';
+button = questdlg(promptMessage,titleBarCaption,'Yes', 'No','Yes');
+if strcmpi(button,'Yes')
+soundsc(y,Fs);
+else
+end
+
+figure;
+xf = applySkiSlope(y, g, frequencys, Fs); % Frequency Shaper
+yt = powerCompress(xf, Psat, Fs); % Amplitude Shaper
+
+% Block to play audio and corresponding graph
+promptMessage=sprintf('Do want to play your recovered file?');
+titleBarCaption='Play';
+button = questdlg(promptMessage,titleBarCaption,'Yes', 'No','Yes');
+if strcmpi(button,'Yes')
+soundsc(yt,Fs);
+else
+end
+figure;
+    plot(yt);
+    title('Last Sound');
